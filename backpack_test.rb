437edcd2a3a867044fc52e1a96ec1435aabd96c3
Don Cooper <don_cooper@hotmail.com>
2017-06-26 11:47:45 -0400

    Wrote tests for gym shoes

--- backpack_test.rb
+++ backpack_test.rb
@@ -43,9 +43,20 @@ class BackpackTest < MiniTest::Test
   end
 
 
-  ###### TESTS ABOUT GYM SHOES... wait, where are the tests about gym shoes? #####
-  # is something missing here?
+  ###### TESTS ABOUT GYM SHOES #####
+  def test_that_packs_gym_shoes_mon_thurs
+    ['monday', 'thursday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      assert(backpack.items.include?('gym shoes'))
+    end
+  end
 
+  def test_that_doesnt_pack_gym_shoes_if_not_mon_thurs
+    ['tuesday', 'wednesday'].each do |day|
+      backpack = build(:backpack, day_of_week: day)
+      refute(backpack.items.include?('gym shoes'))
+    end
+  end
 
   ##### TESTS ABOUT PRINTING THE LIST #####
   def test_that_backpack_prints_correctly
